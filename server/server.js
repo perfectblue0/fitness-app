ebac4e09eb431b622a1f661d38048b5da3b570b5
Paola_G <perfectblue@live.com>
2024-06-20 15:50:48 -0700

    created server folder for back-end, installed express and started to set up server.js, installed dotenv and used in server.js, added dev script for nodemon which was downloaded as well

---
+++ server/server.js
@@ -0,0 +1,13 @@
+require("dotenv").config();
+const express = require("express");
+const PORT = process.env.PORT;
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.json({ mssg: "Hello!" });
+});
+
+app.listen(PORT, () => {
+  console.log(`listening on port ${PORT}`);
+});
